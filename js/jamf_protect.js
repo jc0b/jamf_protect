2e274b6e06e8fb0fd276d439b290cee5b72acb23
jc0b <j@jc0b.computer>
2022-09-04 12:43:09 +0200

    Fix listing date formatting

--- js/jamf_protect.js
+++ js/jamf_protect.js
@@ -1,9 +1,7 @@
 // re-use of Tuxudo's ms_defenderTimestampToMoment in tuxudo/ms_defender
 var jamf_protectTimestampToMoment = function(col, row){
     var cell = $('td:eq('+col+')', row);
-    console.log(cell)
-    //var checkin = parseInt(cell.text()) * 1000;
-    var checkin = parseInt(cell.text());
+    var checkin = parseInt(cell.text()) * 1000;
     if (checkin > 0){
 	console.log("Checkin: " +checkin);
         var date = new Date(checkin);
